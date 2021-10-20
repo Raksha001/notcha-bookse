f18f480abe6e73fdee01cf9c61d78edb9dfa8785
RAKSHA V G <67852393+Raksha001@users.noreply.github.com>
2021-10-20 18:41:45 +0530

    Update Firebase.jsx

--- src/pages/Firebase.jsx
+++ src/pages/Firebase.jsx
@@ -6,13 +6,12 @@ import "firebase/firebase-firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAhDOSfQva09Fr5TdcyrzPbLTkZXjhRFfw",
-  authDomain: "notcha-bookse.firebaseapp.com",
-  projectId: "notcha-bookse",
-  storageBucket: "notcha-bookse.appspot.com",
-  messagingSenderId: "287580267620",
-  appId: "1:287580267620:web:bac6e3309599e2fe0beb71",
-  measurementId: "G-KQMNVPBGJH",
+  apiKey: "AIzaSyCipoBqN81q5fYpjGJwmmRZ71Xmx1hCE64",
+  authDomain: "notcha-bookse-8f064.firebaseapp.com",
+  projectId: "notcha-bookse-8f064",
+  storageBucket: "notcha-bookse-8f064.appspot.com",
+  messagingSenderId: "727381700257",
+  appId: "1:727381700257:web:bec1c484106077fa048ae0"
 };
 
 firebase.initializeApp(firebaseConfig); //func for config
